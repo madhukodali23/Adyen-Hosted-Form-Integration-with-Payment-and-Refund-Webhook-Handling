1a93ddb4156c021fcdb662290feadd509a7017f6
DM_065 <dm_065@Madhu-DM065.local>
2026-05-24 21:12:38 +0530

    changed createConnection to createPool for DB

--- backend/index.js
+++ backend/index.js
@@ -26,18 +26,21 @@ const client = new Client({
 //   database: process.env.DB_NAME,
 // });
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
 
 db.connect((error) => {
   if (error) {
