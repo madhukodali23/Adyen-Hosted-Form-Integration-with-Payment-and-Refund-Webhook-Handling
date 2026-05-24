6be083ed0e1a57db3c825e30311295d6494f387a
DM_065 <dm_065@Madhu-DM065.local>
2026-05-24 17:21:44 +0530

    Moved credentials to env

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,10 @@ const client = new Client({
 
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Madhu@Datman",
-  database: "adyen_payments",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 
