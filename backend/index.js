98e9ca07d0474379b6a9c61d290e252b42fab1cf
DM_065 <dm_065@Madhu-DM065.mp.dev>
2026-05-27 14:08:05 +0530

    added uuid package

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
+const { v4: uuidv4 } =
+  require("uuid");
+
 
 const {
   Client,
